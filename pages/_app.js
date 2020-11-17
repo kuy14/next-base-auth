ce2c8ff260ce13abc0d9c3f4a37a2a7e41f0e92b
Kurniawan Yusuf <kurniawanyusuf.w@gmail.com>
2020-11-17 11:00:42 +0700

    [App] wraping Provider with return statement

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,11 @@ import "semantic-ui-css/semantic.min.css";
 import store from "../store";
 
 function MyApp({ Component, pageProps }) {
-  return;
-  <Provider store={store}>
-    <Component {...pageProps} />;
-  </Provider>;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
 }
 
 export default MyApp;
