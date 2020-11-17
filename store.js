426518438ae92070ac2048450d17dd578f29a827
Kurniawan Yusuf <kurniawanyusuf.w@gmail.com>
2020-11-17 10:26:54 +0700

    [store] Add new store

---
+++ store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+//Add Reducer
+// import testReducer from "./lib/slices/testSlice";
+
+export default configureStore({
+  reducer: {},
+  devTools: true,
+});
